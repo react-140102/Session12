b1f6a91cb28f0e58b07aae7bf713c737cd4e3f83
Pasha <pashaie@gmail.com>
2022-08-09 20:32:40 +0430

    drug detail

--- app09/screens/DrugDetailScreen.tsx
+++ app09/screens/DrugDetailScreen.tsx
@@ -5,9 +5,11 @@ import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
+  ScrollView,
   Text,
   View,
 } from 'react-native';
+import {Card, Paragraph} from 'react-native-paper';
 import api from '../utils/api';
 import {Category} from './Category';
 import {Drug} from './Drug';
@@ -19,7 +21,56 @@ export function DrugDetailScreen({route, navigation}: any) {
   }, []);
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Detail {drug.name}</Text>
+      <ScrollView>
+        <Card>
+          <Card.Title title="نام دارو" />
+          <Card.Content>
+            <Paragraph>{drug.name}</Paragraph>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Card.Title title="موارد مصرف" />
+          <Card.Content>
+            <Paragraph>{drug.usage}</Paragraph>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Card.Title title="موارد مصرف" />
+          <Card.Content>
+            <Paragraph>{drug.usage}</Paragraph>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Card.Title title="موارد مصرف" />
+          <Card.Content>
+            <Paragraph>{drug.usage}</Paragraph>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Card.Title title="موارد مصرف" />
+          <Card.Content>
+            <Paragraph>{drug.usage}</Paragraph>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Card.Title title="موارد مصرف" />
+          <Card.Content>
+            <Paragraph>{drug.usage}</Paragraph>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Card.Title title="موارد مصرف" />
+          <Card.Content>
+            <Paragraph>{drug.usage}</Paragraph>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Card.Title title="موارد مصرف" />
+          <Card.Content>
+            <Paragraph>{drug.usage}</Paragraph>
+          </Card.Content>
+        </Card>
+      </ScrollView>
     </SafeAreaView>
   );
 }
