e03a6e9e14e34de8d26a863e86ea5072adf8a33f
Pasha <pashaie@gmail.com>
2022-08-16 18:50:46 +0430

    asyncStorage

--- app09/screens/DrugDetailScreen.tsx
+++ app09/screens/DrugDetailScreen.tsx
@@ -9,19 +9,53 @@ import {
   Text,
   View,
 } from 'react-native';
-import {Card, Paragraph} from 'react-native-paper';
+import {Button, Card, Paragraph} from 'react-native-paper';
 import api from '../utils/api';
 import {Category} from './Category';
 import {Drug} from './Drug';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export function DrugDetailScreen({route, navigation}: any) {
   const drug: Drug = route.params;
+  const [favorites, setFavorites] = useState<number[]>([]);
   useEffect(() => {
     navigation.setOptions({title: drug.name});
+
+    (async () => {
+      //Serialization / Deserialization
+      setFavorites(
+        JSON.parse((await AsyncStorage.getItem('favorites')) || '[]'),
+      );
+    })();
   }, []);
+
+  useEffect(() => {
+    (async () => {
+      await AsyncStorage.setItem('favorites', JSON.stringify(favorites));
+    })();
+  }, [favorites]);
+
+  const toggleFav = async () => {
+    if (favorites.indexOf(drug.id) > -1) {
+      setFavorites(favorites.filter(x => x !== drug.id));
+    } else {
+      setFavorites([...favorites, drug.id]);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
+        <Button
+          icon={
+            favorites.indexOf(drug.id) > -1
+              ? 'cards-heart'
+              : 'cards-heart-outline'
+          }
+          mode="contained"
+          onPress={toggleFav}>
+          علاقه مندی
+        </Button>
         <Card>
           <Card.Title title="نام دارو" />
           <Card.Content>
