e41be234e4e8559360b453f6bd0cb642b2e8e217
Pasha <pashaie@gmail.com>
2022-08-30 20:18:49 +0430

    payment

--- app09/screens/NewsScreen.tsx
+++ app09/screens/NewsScreen.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, Linking} from 'react-native';
 import {Button} from 'react-native-paper';
 
 export function NewsScreen({navigation}: any) {
+  const payment = () => {
+    Linking.openURL('http://www.mycompany.com/PaymentGateway/send/id=100');
+  };
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Image source={require('../assets/sample.gif')}></Image>
+      <Button onPress={payment}>Pay</Button>
     </View>
   );
 }
